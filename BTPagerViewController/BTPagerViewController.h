023fbb95a28dd7d83cc76d1aa3e3234752c3a538
bati667 <e1106117@gmail.com>
2016-04-12 20:25:49 +0900

    Initial Commit

---
+++ BTPagerViewController/BTPagerViewController.h
@@ -0,0 +1,19 @@
+//
+//  BTPagerViewController.h
+//  BTPagerViewController
+//
+//  Created by Hiroshi Chiba on 2016/04/12.
+//  Copyright © 2016年 Hiroshi Chiba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BTPagerViewController.
+FOUNDATION_EXPORT double BTPagerViewControllerVersionNumber;
+
+//! Project version string for BTPagerViewController.
+FOUNDATION_EXPORT const unsigned char BTPagerViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BTPagerViewController/PublicHeader.h>
+
+
